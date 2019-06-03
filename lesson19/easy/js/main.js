623053b331ce28f9093e5afa16b4b3b559ea57c6
Viktor <viktor.kochegarov@gmail.com>
2019-06-03 21:58:26 +0300

    Add files via upload

---
+++ lesson19/easy/js/main.js
@@ -0,0 +1,26 @@
+var button = document.querySelector('#button')
+var modal = document.querySelector('#modal');
+var close = document.querySelector('#close');
+
+button.addEventListener('click', function(){
+  modal.classList.add('modal_active');
+});
+
+close.addEventListener('click', function(){
+  modal.classList.remove('modal_active');
+});
+
+function autoClose(){
+  modal.classList.remove('modal_active');
+}
+
+setInterval(autoClose, 5000);
+
+$(document).ready(function(){
+  $('h2').css('border', 'solid 3px red');
+});
+
+// p нет, абзатцев нет на странице
+$(function(){
+  $('p').css('border', 'solid 3px red');
+});
